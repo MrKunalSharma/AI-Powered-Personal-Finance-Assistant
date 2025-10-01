a1b8c34b42eee67e9ba4444a48fd5f19556cdf4f
Your Name <your.email@example.com>
2025-10-01 22:22:45 +0530

    Fix Python version issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-import pandas as pd
+# import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
